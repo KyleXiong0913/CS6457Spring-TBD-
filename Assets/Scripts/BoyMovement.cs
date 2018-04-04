7f3c452dcbc36b50f2875e7431ccf71e8a039ad1
Ryan Voor <rvoor3@gatech.edu>
2018-04-04 15:22:46 -0400

    initial commit

---
+++ Assets/Scripts/BoyMovement.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Animations;
+
+public class BoyMovement : MonoBehaviour {
+
+    public float vertical_speed;
+    public float horizontal_speed;
+    public float scaleFactor = 3;
+    private float movespeed;
+    public float rotateDegreesPerSecond = 120.0f;
+    private Vector3 direction = Vector3.zero;
+    private Animator animator;
+    CharacterController CharacterMove;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+        CharacterMove = GetComponent<CharacterController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        vertical_speed = Input.GetAxisRaw("Vertical");
+        horizontal_speed = Input.GetAxisRaw("Mouse X");
+
+        animator.SetFloat("v_speed", vertical_speed);
+
+        if ((horizontal_speed >= 0.05 || horizontal_speed <= -0.05) && (vertical_speed >= 0.1 || vertical_speed <= -0.1))
+        {
+            direction = new Vector3(horizontal_speed, 0, 0);
+            direction = transform.TransformDirection(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction),
+                rotateDegreesPerSecond * Time.deltaTime);
+        } else
+        {
+            Debug.Log(horizontal_speed);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation, 0);
+        }
+        
+    }
+
+
+    private void OnAnimatorMove()
+    {
+        Vector3 newPosition = transform.position;
+        
+        if (vertical_speed >= 0)
+        {
+            movespeed = vertical_speed * scaleFactor;
+        } else
+        {
+            movespeed = vertical_speed;
+        }
+
+        Vector3 moveDirection = transform.forward;
+        CharacterMove.Move(moveDirection * movespeed * Time.deltaTime);
+        //transform.position = newPosition;
+        
+
+    }
+}
+
---
+++ Assets/Scripts/DestructibleCrateSettingsController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestructibleCrateSettingsController : MonoBehaviour {
+
+    // this var will be set in the GUI
+    public float          BreakForce     = Mathf.Infinity;
+    public float          BreakTorque    = Mathf.Infinity;
+    public bool           isKinematic    = false;
+    public PhysicMaterial physicMaterial = null;
+
+	// Use this for initialization
+	void Start () {
+        setForce(BreakForce, BreakTorque);
+        setIsKinematic(isKinematic);
+        setPhysicMaterial(physicMaterial);
+	}
+
+    void setForce(float BreakForce, float BreakTorque) {
+        FixedJoint[] allChildrenJoints = GetComponentsInChildren<FixedJoint>();
+        foreach (FixedJoint fixedJoint in allChildrenJoints) {
+            fixedJoint.breakForce  = BreakForce;
+            fixedJoint.breakTorque = BreakForce;
+        }
+    }
+
+    void setIsKinematic(bool isKinematic) {
+        Rigidbody[] allChildrenRigidBodies = GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rigidBody in allChildrenRigidBodies) {
+            rigidBody.isKinematic = isKinematic;
+        }
+    }
+
+    void setPhysicMaterial(PhysicMaterial physicMaterial) {
+        BoxCollider[] allChildrenBoxColliders = GetComponentsInChildren<BoxCollider>();
+        foreach (BoxCollider boxCollider in allChildrenBoxColliders) {
+            boxCollider.material = physicMaterial;
+        }
+    }
+}
