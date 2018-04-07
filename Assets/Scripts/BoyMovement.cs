bd8faf60e649c95bd692e796f77f4b7720f7ae5c
Alex Loomis <alex.v.loomis@gmail.com>
2018-04-07 16:47:42 -0400

    Player Character updated animations & bat swinging!
    - 2 Scripts: BoyMovement & CreateHitForce (both with lots of comments explaining how they work)
    
    - Player has a bat that can knock down the destructible environment (it is a child of the player’s right hand)
    
    - To make the bat movements more forceful, added extra collider in front of player that applies force when the character swings.
    
    - Character animations are split into upper and lower body movements (so the character can swing while moving)
    
    - Can change strength of character swings based on HitForce object with CreateHitForce object on it (has a scalar that changes magnitude of force applied)
    
    - Character can both turn in place as well as run and turn. Camera is always fixed position behind player.
    
    - I included the Baseball Bats folder which houses the materials and textures for a ton of different styles of baseball bat, in case you guys didn’t like the one I picked. (BY NO MEANS SHOULD WE KEEP ALL OF THEM)
    
    - There is a pretty major change (though easy in implementation) that I did for the character: I changed his physics layer to “Player”. I also went ahead and made sure that the Player physics layer cannot collide with itself. This is to keep the bat from just breaking out of his hands. It also makes sure that objects colliding with that extra “hit box” for bat swings aren’t actually the player himself or the bat.
    This is very easy to implement, you just have to make sure that when you change aj_prefab to have its layer be “Player” (should be provided by Unity) that you say yes when it asks if you want that to cascade to its children. This way the layer will be applied to every node in the player’s body, including the bat which is a child of the right hand!

--- Assets/Scripts/BoyMovement.cs
+++ Assets/Scripts/BoyMovement.cs
@@ -5,60 +5,93 @@ using UnityEditor.Animations;
 
 public class BoyMovement : MonoBehaviour {
 
-    public float vertical_speed;
-    public float horizontal_speed;
-    public float scaleFactor = 3;
-    private float movespeed;
-    public float rotateDegreesPerSecond = 120.0f;
-    private Vector3 direction = Vector3.zero;
-    private Animator animator;
-    CharacterController CharacterMove;
+    public GameObject bat;
+    public GameObject hitForce; // collider that adds extra force after swing
+    public FixedJoint handBatJoint; // fixed joint from right hand to bat
+    public Rigidbody batRigidbody;
 
-    // Use this for initialization
+    private Animator animator; // player animator controller
+
+    private Vector3 direction; // used for player rotation
+    private Vector3 batIdleTrans = new Vector3(0.104f, -0.062f, -0.148f); // local position of bat while idle
+    private Vector3 batIdleRot = new Vector3(-78.928f, 211.185f, -31.269f); // local rotation of bat while idle
+    private Vector3 batSwingTrans = new Vector3(0.0888f, -0.0301f, -0.1503f); // local position of bat while swinging
+    private Vector3 batSwingRot = new Vector3(-58.624f, 351.56f, -173.957f); // local rotation of bat while swinging
+
+    private float vertical_speed; // sent to animator controller - forward motion
+    private float horizontal_speed; // sent to animator controller - turning motion
+    private float rotateDegreesPerSecond = 120.0f;
+
+
     void Start () {
         animator = GetComponent<Animator>();
-        CharacterMove = GetComponent<CharacterController>();
+        hitForce.GetComponent<CapsuleCollider>().enabled = false;
+        direction = Vector3.zero;
     }
 	
-	// Update is called once per frame
+
 	void Update () {
         vertical_speed = Input.GetAxisRaw("Vertical");
-        horizontal_speed = Input.GetAxisRaw("Mouse X");
+        horizontal_speed = Input.GetAxisRaw("Mouse X"); // corresponds to right joystick X-axis on controller
 
-        animator.SetFloat("v_speed", vertical_speed);
+        animator.SetFloat("v_speed", vertical_speed); // send forward movement to AC
+
+        if (Input.GetKeyDown("joystick 1 button 0")) // if A button pressed, perform a swing
+        {
+            animator.SetTrigger("swing");
+
+            // The bat needs a different pos & rot relative to the parent hand when swinging, so below
+            // we disconnet the fixed joint in order to change these, and then reattach the bat to the hand.
+            handBatJoint.connectedBody = null;
+            bat.transform.localPosition = batSwingTrans; 
+            Quaternion rotation = Quaternion.Euler(batSwingRot);
+            bat.transform.localRotation = Quaternion.RotateTowards(bat.transform.localRotation, rotation,
+                1000 * Time.deltaTime); // This particular operation accounts for the animation transition
+            handBatJoint.connectedBody = batRigidbody;
+        }
 
         if ((horizontal_speed >= 0.05 || horizontal_speed <= -0.05) && (vertical_speed >= 0.1 || vertical_speed <= -0.1))
         {
+            // Turns the character based on right stick. Doesn't affect animations,
+            // but does affect the camera since it follows the player.
+            animator.SetBool("turningInPlace", false);
             direction = new Vector3(horizontal_speed, 0, 0);
             direction = transform.TransformDirection(direction);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(direction),
                 rotateDegreesPerSecond * Time.deltaTime);
-        } else
+        } else if ((horizontal_speed >= 0.05 || horizontal_speed <= -0.05) && (vertical_speed <= 0.1 || vertical_speed >= -0.1))
         {
+            // Turns the character in place if there is no forward movement.
+            animator.SetBool("turningInPlace", true);
+            animator.SetFloat("h_speed", horizontal_speed*10);
             Debug.Log(horizontal_speed);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, transform.rotation, 0);
-        }
-        
-    }
-
-
-    private void OnAnimatorMove()
-    {
-        Vector3 newPosition = transform.position;
-        
-        if (vertical_speed >= 0)
-        {
-            movespeed = vertical_speed * scaleFactor;
         } else
         {
-            movespeed = vertical_speed;
+            animator.SetBool("turningInPlace", false);
         }
 
-        Vector3 moveDirection = transform.forward;
-        CharacterMove.Move(moveDirection * movespeed * Time.deltaTime);
-        //transform.position = newPosition;
+        if (animator.GetCurrentAnimatorStateInfo(1).IsName("Swing"))
+        {
+            // The bat needs a different pos & rot relative to the parent hand when swinging, so below
+            // we disconnet the fixed joint in order to change these, and then reattach the bat to the hand.
+            handBatJoint.connectedBody = null;
+            bat.transform.localPosition = batSwingTrans;
+            Quaternion rotation = Quaternion.Euler(batSwingRot);
+            bat.transform.localRotation = rotation;
+            handBatJoint.connectedBody = batRigidbody;
+            hitForce.GetComponent<CapsuleCollider>().enabled = true;
+        } else
+        {
+            // The bat needs a different pos & rot relative to the parent hand when swinging, so below
+            // we disconnet the fixed joint in order to change these, and then reattach the bat to the hand.
+            handBatJoint.connectedBody = null;
+            bat.transform.localPosition = batIdleTrans;
+            Quaternion rotation = Quaternion.Euler(batIdleRot);
+            bat.transform.localRotation = rotation;
+            handBatJoint.connectedBody = batRigidbody;
+            hitForce.GetComponent<CapsuleCollider>().enabled = false;
+        }
         
-
     }
 }
 
---
+++ Assets/Scripts/CreateHitForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateHitForce : MonoBehaviour {
+
+    public float scaleFactor = 300.0f; // Changes how strong the applied force is
+
+
+    void Start () {
+    }
+
+    // This collider will apply a force to everything inside it when a swing happens
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.layer != 8) // ensures the collider cannot add force to the player
+        {
+            Rigidbody body = collider.gameObject.GetComponent<Rigidbody>();
+            Vector3 direction = collider.gameObject.transform.position - transform.position;
+            body.AddForceAtPosition(direction * scaleFactor, transform.position);
+        }   
+    }
+}
