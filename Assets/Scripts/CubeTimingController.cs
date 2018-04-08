64197dd9d265aaef3ec1c71c75913dd6f94576b1
Ryan Voor <rvoor3@gatech.edu>
2018-04-07 20:39:09 -0400

    merged in the new player character and also created a more efficient destructible environment

---
+++ Assets/Scripts/CubeTimingController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeTimingController : MonoBehaviour {
+
+    Rigidbody  rigidBody;
+    bool       activated;
+    bool       wasActivatedAtSomePoint = false;
+    int        timeSinceActivation = 0;
+    public int maxEnabledTime      = 300;
+
+	// Use this for initialization
+	void Start () {
+        rigidBody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        if (activated) {
+            timeSinceActivation++;
+        }
+        if (timeSinceActivation >= maxEnabledTime) {
+            DisableCube();
+        }
+	}
+
+    // my functions
+    // will happen when a cube collides with the player
+    void EnableCube() {
+        rigidBody.isKinematic      = false;
+        rigidBody.detectCollisions = true;
+        activated                  = true;
+        wasActivatedAtSomePoint    = true;
+    }
+
+    // will happen after some set amount of time from when the cube collides with the player
+    void DisableCube() {
+        rigidBody.isKinematic      = true;
+        rigidBody.detectCollisions = false;
+        activated                  = false;
+    }
+
+    void OnTriggerEnter(Collider otherObjectCollider) {
+        if (otherObjectCollider.gameObject.tag == "Player" && !wasActivatedAtSomePoint) {
+            EnableCube();
+        }
+    }
+}
